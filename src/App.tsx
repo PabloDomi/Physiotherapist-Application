17db173310ed5fddd5da6cec4375b4ab284c0911
PabloDomiBISITE <domi@usal.es>
2024-04-12 10:44:23 +0200

    Estado global de zustand añadido view, user, changeView. Añadidas rutas para Landing de primeras y Home una vez tenemos usuario. Tambien añadida la ruta de Not Found

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { ThemeContext } from './Services/themeProvider'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
 import { Estadisticas } from './Views/Estadisticas'
 import { NavBar } from './Components/Navbar'
 import { BrandName } from './Utils/Constants'
@@ -9,37 +9,61 @@ import { Rutinas } from './Views/Rutinas'
 import { Ejercicios } from './Views/Ejercicios'
 import { ButtonToggleTheme } from './Components/ButtonToggleTheme'
 import './index.css'
+import { useGlobalState } from './Store/useGlobalState'
 
 function App() {
 
+  const location = useLocation()
+
   // Context for Theme in App
   const { theme, toggleTheme } = useContext(ThemeContext)
+  const user = useGlobalState(state => state.user)
+  const changeView = useGlobalState(state => state.changeView)
+  const view = useGlobalState(state => state.view)
+
+  useEffect(() => {
+    const newLocation = location.pathname.slice(1)
+    changeView(newLocation)
+  }, [changeView, location.pathname])
+
+  console.log('Estamos en: ' + view)
+
 
   return (
     <>
-      <NavBar
-        styledClassName={theme === 'light'
-          ? "navbar-light bg-white shadow pe-3"
-          : "navbar-dark bg-dark shadow pe-3"
-        }
-        brandName={BrandName}
-        imageSrcPath={imagePath}
-      />
-      <Routes>
-        <Route path='/' element={<Estadisticas theme={theme} />} />
-        <Route path='/rutinas' element={<Rutinas />} />
-        <Route path='/ejercicios' element={<Ejercicios />} />
-      </Routes>
+      {!user
+        ?
+        <>
+          <NavBar
+            styledClassName={theme === 'light'
+              ? "navbar-light bg-white shadow pe-3"
+              : "navbar-dark bg-dark shadow pe-3"
+            }
+            brandName={BrandName}
+            imageSrcPath={imagePath}
+          />
+          <Routes>
+            <Route path='/' element={<Navigate to={'/home'} replace={true} />} />
+            <Route path='/home' element={<Estadisticas theme={theme} />} />
+            <Route path='/rutinas' element={<Rutinas />} />
+            <Route path='/ejercicios' element={<Ejercicios />} />
+            <Route path='*' element={<h1>Not Found!</h1>} />
+          </Routes>
 
-      <footer className={theme === 'light'
-        ? 'footer-dark'
-        : 'footer-light'
-      }>
-        <div className='footer-container'>
-          <p className='footer-text font-tilt-neon'>Derechos de autor © 2024</p>
-        </div>
-      </footer>
-      <ButtonToggleTheme toggleTheme={toggleTheme} theme={theme} />
+          <footer className={theme === 'light'
+            ? 'footer-dark'
+            : 'footer-light'
+          }>
+            <div className='footer-container'>
+              <p className='footer-text font-tilt-neon'>Derechos de autor © 2024</p>
+            </div>
+          </footer>
+          <ButtonToggleTheme toggleTheme={toggleTheme} theme={theme} />
+        </>
+        : <Routes>
+          <Route path='/' element={<h1>Landing</h1>} />
+        </Routes>
+      }
     </>
   )
 }
---
+++ src/Store/useGlobalState.tsx
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+import { GlobalState } from "../Utils/types";
+
+export const useGlobalState = create<GlobalState>((set) => {
+  return {
+    view: '',
+    user: null,
+
+    changeView: (newView: string) => set({ view: newView }),
+  }
+});
